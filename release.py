eb2c8b5b885cd5532c9b500348614c02d87d674a
aeon0 <aeon4@pm.me>
2021-11-05 08:36:46 +0100

    fix(release): Make preivous build folder deletition work

--- release.py
+++ release.py
@@ -1,7 +1,7 @@
 import os
 import shutil
-import argparse
-import sys
+from pathlib import Path
+import glob
 from src.version import __version__
 
 # Note: Before building you must add cv2 to python path:
@@ -21,12 +21,19 @@ def clean_up():
         os.remove("run.exe")
 
 clean_up()
-installer_cmd = f"python -m nuitka --mingw --standalone --onefile --plugin-enable=numpy src/run.py" 
-os.system(installer_cmd)
 
 botty_dir = f"botty_v{__version__}"
 if os.path.exists(botty_dir):
-    os.remove(botty_dir)
+    for path in Path(botty_dir).glob("**/*"):
+        if path.is_file():
+            os.remove(path)
+        elif path.is_dir():
+            shutil.rmtree(path)
+    shutil.rmtree(botty_dir)
+
+installer_cmd = f"python -m nuitka --mingw --standalone --onefile --plugin-enable=numpy src/run.py" 
+os.system(installer_cmd)
+
 os.system(f"mkdir {botty_dir}")
 shutil.copy("run.exe", f"{botty_dir}/")
 shutil.copy("ui.ini", f"{botty_dir}/")
