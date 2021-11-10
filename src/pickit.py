6e5ce40ba71b2f41fd31a04d8bdefed73df8f8a4
aeon0 <aeon4@pm.me>
2021-11-10 20:30:30 +0100

    Update src/pickit.py

--- src/pickit.py
+++ src/pickit.py
@@ -47,7 +47,7 @@ class PickIt:
                 x_m, y_m = self._screen.convert_screen_to_monitor(closest_item.center)
                 if closest_item.dist < self._config.ui_pos["item_dist"]:
                     # no need to stash potions, scrolls, or gold 
-                    if (("potion" not in closest_item.name) and ("tp_scroll" != closest_item.name) and ("gold" not in closest_item.name)):
+                    if (("potion" not in closest_item.name) and ("tp_scroll" != closest_item.name) and ("misc_gold" not in closest_item.name)):
                         found_items = True
                         Logger.info(f"** Looted {closest_item.name}")
                     else:
