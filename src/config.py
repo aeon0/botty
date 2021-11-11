ee9f08629acbd9451a00c12ffb58aa93d3aad7c8
aeon0 <aeon4@pm.me>
2021-11-12 00:39:01 +0100

    missing es_available in config.py

--- src/config.py
+++ src/config.py
@@ -56,6 +56,7 @@ class Config:
             "potion2": self._select_val("char", "potion2"),
             "potion3": self._select_val("char", "potion3"),
             "potion4": self._select_val("char", "potion4"),
+            "es_available": self._select_val("char", "es_available"),
             "cta_available": bool(int(self._select_val("char", "cta_available"))),
             "weapon_switch": self._select_val("char", "weapon_switch"),
             "battle_orders": self._select_val("char", "battle_orders"),
