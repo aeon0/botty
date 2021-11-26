bc0aebf4428bf70fdfb63c28c5ff80955fdb435d
aeon0 <aeon4@pm.me>
2021-11-26 18:48:20 +0100

    Actually use the failsave for red portal miss (#123)

--- src/bot.py
+++ src/bot.py
@@ -260,7 +260,7 @@ class Bot:
                     return
                 bot._curr_location = Location.NIHLATHAK_PORTAL
                 wait(0.2, 0.4)
-                self.success &= bot._char.select_by_template("A5_RED_PORTAL")
+                self.success &= bot._char.select_by_template(["A5_RED_PORTAL", "A5_RED_PORTAL_TEXT"], expect_loading_screen=True)
                 time.sleep(0.5)
                 self.success &= bot._template_finder.search_and_wait(["PINDLE_0", "PINDLE_1"], threshold=0.65, time_out=20)[0]
                 if not self.success:
