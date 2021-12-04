49696aeae8cc396c84b5a441cedee9fd964e2210
Matt <rogue_drunkerd@hotmail.com>
2021-12-04 15:55:57 -0600

    forgot to remove comment after testing

--- src/bot.py
+++ src/bot.py
@@ -133,7 +133,7 @@ class Bot:
             Logger.debug(f"Found {template_match.name}")
         if not self._ui_manager.start_game():
             return
-        # self._template_finder.search_and_wait(["A5_TOWN_1", "A5_TOWN_0"])
+        self._template_finder.search_and_wait(["A5_TOWN_1", "A5_TOWN_0"])
         self._tp_is_up = False
         self._curr_location = Location.A5_TOWN_START
         # Make sure these keys are released
