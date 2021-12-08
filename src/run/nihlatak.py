6f1c6edf59e3a8d1de22af1db4657d4f707bfa5b
jobithu <95491220+jobithu@users.noreply.github.com>
2021-12-08 22:35:46 +0100

    fixed some issues on template syntax

--- src/run/nihlatak.py
+++ src/run/nihlatak.py
@@ -39,7 +39,7 @@ class Nihlatak:
     def battle(self, do_nihlatak: bool, do_pre_buff: bool) -> Union[bool, tuple[Location, bool]]:
         if do_pre_buff: # we buff first, to get that out of the way of the whole logic that is to follow for finding the right way
             self._char.pre_buff()        
-        template_match = self._template_finder.search_and_wait(["NI1_A", "NI1_B", "NI1_C", threshold=0.65, time_out=20) # So let's check which layout ("NI1_A = bottom exit" , "NI1_B = large room", "NI1_C = small room") of Level1 we have.
+        template_match = self._template_finder.search_and_wait(["NI1_A", "NI1_B", "NI1_C"], threshold=0.65, time_out=20) # So let's check which layout ("NI1_A = bottom exit" , "NI1_B = large room", "NI1_C = small room") of Level1 we have.
         if not template_match.valid:# check if any of these templates was found
             return False
         self._pather.traverse_nodes_fixed(template_match.name.lower(), self._char) # depending on what template is found we do static pathing to the stairs on level1. It expects that the static routes to be defined in game.ini named: "ni_a", "ni_b", "ni_c"
@@ -56,20 +56,20 @@ class Nihlatak:
 
 
         pather.traverse_nodes(Location.A5_NIHLATAK_LVL2_START, Location.A5_NIHLATAK_LVL2_A, char) # So now we traverse from stairs to the first location to check for eyes: brings us from stairs to eye check A
-        template_match = self._template_finder.search_and_wait(["NI2_A", threshold=0.65, time_out=20)  # look for the eye at location A
+        template_match = self._template_finder.search_and_wait(["NI2_A"], threshold=0.65, time_out=20)  # look for the eye at location A
         if template_match.valid: 
             self._pather.traverse_nodes_fixed(template_match.name.lower(), self._char) # I found the eye and now moving on to nihlatak. 
         elif #did not find the eye in position A, so lets move on
             pather.traverse_nodes(Location.A5_NIHLATAK_LVL2_A, Location.A5_NIHLATAK_LVL2_B, char) #brings us from eye check A to eye check B
-            template_match = self._template_finder.search_and_wait(["NI2_B", threshold=0.65, time_out=20)  # look for the eye at location B
+            template_match = self._template_finder.search_and_wait(["NI2_B"], threshold=0.65, time_out=20)  # look for the eye at location B
             self._pather.traverse_nodes_fixed(template_match.name.lower(), self._char) #path to nihlatak in position B
         elif #did not find the eye in position B, so lets move on
             pather.traverse_nodes(Location.A5_NIHLATAK_LVL2_B, Location.A5_NIHLATAK_LVL2_C, char) #brings us from eye check B to eye check C
-            template_match = self._template_finder.search_and_wait(["NI2_C", threshold=0.65, time_out=20)  # look for the eye at location C
+            template_match = self._template_finder.search_and_wait(["NI2_C"], threshold=0.65, time_out=20)  # look for the eye at location C
             self._pather.traverse_nodes_fixed(template_match.name.lower(), self._char) #path to nihlatak in position C
         elif #did not find the eye in position C, so lets move on
             pather.traverse_nodes(Location.A5_NIHLATAK_LVL2_C, Location.A5_NIHLATAK_LVL2_D, char) #brings us from eye check C to eye check D
-            template_match = self._template_finder.search_and_wait(["NI2_D", threshold=0.65, time_out=20)  # look for the eye at location D
+            template_match = self._template_finder.search_and_wait(["NI2_D"], threshold=0.65, time_out=20)  # look for the eye at location D
             self._pather.traverse_nodes_fixed(template_match.name.lower(), self._char) #path to nihlatak in position D
         else #did not find the eye in position D, so lets move on
             pather.traverse_nodes(Location.A5_NIHLATAK_LVL2_D, Location.A5_NIHLATAK_LVL2_END, char) #brings us from eye check D back to the stairs - if I end up here, then I didnt find an eye and can go back to Level1 for save TP home.
