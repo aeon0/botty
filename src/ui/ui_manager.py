49462b94cf667af8b94daee8fa201f335429be48
aeon0 <aeon4@pm.me>
2021-12-12 19:32:48 +0100

    stash optimization

--- src/ui/ui_manager.py
+++ src/ui/ui_manager.py
@@ -325,19 +325,15 @@ class UiManager():
                     Logger.info("Stash tab is full of gold, selecting next stash tab.")
                     self._curr_stash["gold"] += 1
                     if self._curr_stash["gold"] > 3:
-                        inventory_full_gold = self._template_finder.search("INVENTORY_FULL_GOLD", self._screen.grab(), roi=self._config.ui_roi["inventory_gold"], threshold=0.98)
-                        if inventory_full_gold.valid:
-                            # turn of gold pickup
-                            self._config.items["misc_gold"] = 0
-                            item_finder.update_items_to_pick(self._config)
-                            # inform user about it
-                            msg = "All stash tabs and character are full of gold, turn of gold pickup"
-                            Logger.info(msg)
-                            if self._config.general["custom_message_hook"]:
-                                self._messenger.send(msg=f"{self._config.general['name']}: {msg}")
-                        else:
-                            Logger.info("All tabs are full but character is not. Continuing.")
-                            self._curr_stash["gold"] = 3
+                        # turn of gold pickup
+                        self._config.char["stash_gold"] = False
+                        self._config.items["misc_gold"] = False
+                        item_finder.update_items_to_pick(self._config)
+                        # inform user about it
+                        msg = "All stash tabs and character are full of gold, turn of gold pickup"
+                        Logger.info(msg)
+                        if self._config.general["custom_message_hook"]:
+                            self._messenger.send(msg=f"{self._config.general['name']}: {msg}")
                     else:
                         # move to next stash
                         wait(0.5, 0.6)
