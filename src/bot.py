1a211736e12723a8af1c1495a1540bd60cbbe2b8
egut125 <egut125@gmail.com>
2021-12-19 02:26:40 -0500

    Correctly Total Failed Runs, Only Set location to shenk when battling, not on run start (#303)

--- src/bot.py
+++ src/bot.py
@@ -310,7 +310,6 @@ class Bot:
     def on_run_shenk(self):
         res = False
         self._do_runs["run_shenk"] = False
-        self._game_stats.update_location("Shenk")
         self._curr_loc = self._shenk.approach(self._curr_loc)
         if self._curr_loc:
             res = self._shenk.battle(self._route_config["run_shenk"], not self._pre_buffed, self._game_stats)
