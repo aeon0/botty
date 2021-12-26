3bb1124177c0be7310e11a9a04f2589a8d351dd1
Luiz Felipe <kozfelipe@gmail.com>
2021-12-26 01:07:39 -0300

    chest management

--- src/run/arcane.py
+++ src/run/arcane.py
@@ -8,8 +8,8 @@ from template_finder import TemplateFinder
 from town.town_manager import TownManager
 from ui import UiManager
 from utils.misc import wait
-from dataclasses import dataclass
-import os
+from screen import Screen
+from chest import Chest
 
 class Arcane:
     def __init__(
@@ -28,22 +28,8 @@ class Arcane:
         self._ui_manager = ui_manager
         self._char = char
         self._pickit = pickit
-        
-    def _open_stashes(self) -> bool:
-        for filename in os.listdir(f'assets/templates/arcane/stashes'):
-            filename = filename.lower()
-            if filename.endswith('.png'):
-                stash = filename[:-4].upper()
-                if self._char.select_by_template([stash], None, 2):
-                    wait(1, 2)
-        for filename in os.listdir(f'assets/templates/arcane/stashes'):
-            filename = filename.lower()
-            if filename.endswith('.png'):
-                stash = filename[:-4].upper()
-                if self._char.select_by_template([stash], None, 1):
-                    wait(0.5, 1)
-        return True
-
+        self._chest = Chest(self._char, self._template_finder, 'arcane')
+    
     def approach(self, start_loc: Location) -> Union[bool, Location]:
         Logger.info("Run Arcane")
         #if not self._char.can_teleport():
@@ -74,9 +60,8 @@ class Arcane:
         # Attack
         self._char.kill_summoner()
 
-        # Stashes & Pick items
-        self._open_stashes()
-        wait(0.2, 0.3)
+        # Chests & Pick items
+        self._chest.open_up_chests()
         picked_up_items = self._pickit.pick_up_items(self._char)
         
         return (Location.A2_ARC_END, picked_up_items)
