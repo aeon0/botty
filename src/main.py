027b153c6d379825657771b8e30e8de8b6550815
Francesco <francesco.zini1@icloud.com>
2021-12-30 21:15:01 +0000

    Refactored graphic debugger with similar logic to botty to now have a controller.

--- src/main.py
+++ src/main.py
@@ -8,22 +8,40 @@ import traceback
 
 from game_controller import GameController
 from version import __version__
-from utils.graphic_debugger import run_graphic_debugger
+from utils.graphic_debugger import GraphicDebuggerController
 from utils.auto_settings import adjust_settings, backup_settings, restore_settings_from_backup
 
 from config import Config
 from logger import Logger
 
+config = Config(print_warnings=True)
+game_controller = GameController(config)
+debugger_controller = GraphicDebuggerController(config)
 
-def start_bot(config: Config):
-    game_controller = GameController(config)
-    game_controller_thread = threading.Thread(target=game_controller.run_bot)
-    game_controller_thread.daemon = True
-    game_controller_thread.start()
+
+def start_or_pause_bot():
+    global game_controller
+    global debugger_controller
+    if GameController.is_running:
+        game_controller.toggle_pause_bot()
+    else:
+        # Kill the debugger if we invoke botty
+        debugger_controller.stop()
+        game_controller.start()
+
+
+def start_or_stop_graphic_debugger():
+    global game_controller
+    global debugger_controller
+    if GraphicDebuggerController.is_running:
+        debugger_controller.stop()
+    else:
+        # Kill botty if we invoke the debugger
+        game_controller.stop()
+        debugger_controller.start()
 
 
 def main():
-    config = Config(print_warnings=True)
     if config.general["logg_lvl"] == "info":
         Logger.init(logging.INFO)
     elif config.general["logg_lvl"] == "debug":
@@ -52,10 +70,10 @@ def main():
     print("\n")
 
     keyboard.add_hotkey(config.general['auto_settings_key'], lambda: adjust_settings(config))
-    keyboard.add_hotkey(config.general['graphic_debugger_key'], lambda: run_graphic_debugger())
+    keyboard.add_hotkey(config.general['graphic_debugger_key'], lambda: start_or_stop_graphic_debugger())
     keyboard.add_hotkey(config.general['restore_settings_from_backup_key'], lambda: restore_settings_from_backup(config))
     keyboard.add_hotkey(config.general['settings_backup_key'], lambda: backup_settings(config))
-    keyboard.add_hotkey(config.general['resume_key'], lambda c: start_bot(c), args=[config])
+    keyboard.add_hotkey(config.general['resume_key'], lambda c: start_or_pause_bot(), args=[config])
     keyboard.add_hotkey(config.general["exit_key"], lambda: Logger.info(f'Force Exit') or os._exit(1))
     keyboard.wait()
     print('stopped waiting')
--- src/utils/graphic_debugger.py
+++ src/utils/graphic_debugger.py
@@ -1,53 +1,69 @@
+import threading
+
 import cv2
 import numpy as np
-from utils.misc import color_filter
+from utils.misc import color_filter, kill_thread
 from screen import Screen
 from item import ItemFinder
 from config import Config
 from template_finder import TemplateFinder
 
-running = False
 
+class GraphicDebuggerController:
+    is_running = False
 
-def run_graphic_debugger():
-    global running
-    if running:
-        return
-    running = True
-    config = Config()
-    screen = Screen(config.general["monitor"])
-    item_finder = ItemFinder(config)
-    template_finder = TemplateFinder(screen)
-    search_templates = ["A5_TOWN_0", "A5_TOWN_1", "A5_TOWN_2", "A5_TOWN_3"]
-    while 1:
-        img = screen.grab()
-        # Show item detections
-        combined_img = np.zeros(img.shape, dtype="uint8")
-        for key in config.colors:
-            _, filterd_img = color_filter(img, config.colors[key])
-            combined_img = cv2.bitwise_or(filterd_img, combined_img)
-        item_list = item_finder.search(img)
-        for item in item_list:
-            cv2.circle(combined_img, item.center, 7, (0, 0, 255), 4)
-            cv2.putText(combined_img, item.name, item.center, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
-        if len(item_list) > 0:
-            print(item_list)
-        # Show Town A5 template matches
-        scores = {}
-        for template_name in search_templates:
-            template_match = template_finder.search(template_name, img, threshold=0.65)
-            if template_match.valid:
-                scores[template_match.name] = template_match.score
-                cv2.putText(combined_img, str(template_name), template_match.position, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-                cv2.circle(combined_img, template_match.position, 7, (255, 0, 0), thickness=5)
-        if len(scores) > 0:
-            print(scores)
-        # Show img
-        combined_img = cv2.resize(combined_img, None, fx=0.5, fy=0.5)
-        cv2.imshow("debug img", combined_img)
-        cv2.setWindowProperty("debug img", cv2.WND_PROP_TOPMOST, 1)
-        cv2.waitKey(1)
+    def __init__(self, config: Config):
+        self.config = config
+        self.screen = None
+        self.item_finder = None
+        self.template_finder = None
+        self.debugger_thread = None
+
+    def start(self):
+        self.item_finder = ItemFinder(self.config)
+        self.screen = Screen(self.config.general["monitor"])
+        self.template_finder = TemplateFinder(self.screen)
+        self.debugger_thread = threading.Thread(target=self.run_debugger)
+        self.debugger_thread.daemon = False
+        self.debugger_thread.start()
+        GraphicDebuggerController.is_running = True
+
+    def stop(self):
+        if self.debugger_thread: kill_thread(self.debugger_thread)
+        GraphicDebuggerController.is_running = False
+
+    def run_debugger(self):
+        search_templates = ["A5_TOWN_0", "A5_TOWN_1", "A5_TOWN_2", "A5_TOWN_3"]
+        while 1:
+            img = self.screen.grab()
+            # Show item detections
+            combined_img = np.zeros(img.shape, dtype="uint8")
+            for key in self.config.colors:
+                _, filterd_img = color_filter(img, self.config.colors[key])
+                combined_img = cv2.bitwise_or(filterd_img, combined_img)
+            item_list = self.item_finder.search(img)
+            for item in item_list:
+                cv2.circle(combined_img, item.center, 7, (0, 0, 255), 4)
+                cv2.putText(combined_img, item.name, item.center, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+            if len(item_list) > 0:
+                print(item_list)
+            # Show Town A5 template matches
+            scores = {}
+            for template_name in search_templates:
+                template_match = self.template_finder.search(template_name, img, threshold=0.65)
+                if template_match.valid:
+                    scores[template_match.name] = template_match.score
+                    cv2.putText(combined_img, str(template_name), template_match.position, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                    cv2.circle(combined_img, template_match.position, 7, (255, 0, 0), thickness=5)
+            if len(scores) > 0:
+                print(scores)
+            # Show img
+            combined_img = cv2.resize(combined_img, None, fx=0.5, fy=0.5)
+            cv2.imshow("debug img", combined_img)
+            cv2.setWindowProperty("debug img", cv2.WND_PROP_TOPMOST, 1)
+            cv2.waitKey(1)
 
 
 if __name__ == "__main__":
-    run_graphic_debugger()
+    debugger = GraphicDebuggerController(Config())
+    debugger.start()
