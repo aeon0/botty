b8c3fd1f54657dee6cafc15f7d6853af3ef2ae0b
aeon0 <aeon4@pm.me>
2021-12-31 14:18:55 +0100

    with old pathing

--- src/run/arcane.py
+++ src/run/arcane.py
@@ -30,6 +30,7 @@ class Arcane:
         self._char = char
         self._pickit = pickit
         self._chest = Chest(self._char, self._template_finder, 'arcane')
+        self.used_tps = 0
 
     def approach(self, start_loc: Location) -> Union[bool, Location]:
         Logger.info("Run Arcane")
@@ -42,11 +43,13 @@ class Arcane:
         return Location.A2_ARC_START
 
     def _find_summoner(self, traverse_to_summoner: list[tuple[float, float]]) -> bool:
+        # Try to calibarte at center of platform
+        self._pather.traverse_nodes([462], self._char, time_out=1.0)
         # Check if we arrived at platform
         template_match_platform = self._template_finder.search_and_wait(["ARC_PLATFORM_1", "ARC_PLATFORM_2", "ARC_PLATFORM_3", "ARC_CENTER"], threshold=0.55, time_out=0.6, take_ss=False)
         if template_match_platform.valid:
             # Check if we found Summoner
-            template_match_summoner = self._template_finder.search_and_wait(["ARC_ALTAR", "ARC_ALTAR2"], threshold=0.70, time_out=0.6)
+            template_match_summoner = self._template_finder.search_and_wait(["ARC_ALTAR", "ARC_ALTAR2", "ARC_END_STAIRS"], threshold=0.77, time_out=0.6)
             if template_match_summoner.valid:
                 # Seems like we jumped right anot the platform
                 if self._pather.traverse_nodes([461], self._char, time_out=2.2, force_tp=True):
@@ -54,23 +57,9 @@ class Arcane:
             return False
         # We might have arrived at summoner, move up stairs with static traverse
         self._pather.traverse_nodes_fixed(traverse_to_summoner, self._char)
-        if self._pather.traverse_nodes([461], self._char, time_out=2.2, force_tp=True):
+        if self._pather.traverse_nodes([461], self._char, time_out=1.0, force_tp=True):
             return True
         return False
-
-    def _return_to_wp(self, calib_node: int, path: str, traverse = list[tuple[float, float]]) -> bool:
-        Logger.debug("Returning to wp")
-        if not self._pather.traverse_nodes([calib_node], self._char, force_tp=True):
-            Logger.debug("Could not find calibration node")
-            return False
-        if not self._pather.traverse_nodes_fixed(path, self._char):
-            return False
-        if not self._pather.traverse_nodes_fixed(traverse, self._char):
-            return False
-        if not self._template_finder.search_and_wait(["ARC_START"], threshold=0.70, time_out=3.5):
-            Logger.debug("Could not find start template")
-            return False
-        return True
 
     def battle(self, do_pre_buff: bool) -> Union[bool, tuple[Location, bool]]:
         picked_up_items = False
@@ -81,22 +70,20 @@ class Arcane:
             calib_jump_start: list[tuple[float, float]]
             static_path_forward: str
             jump_to_summoner: list[tuple[float, float]]
-            calib_node_back: int
-            static_path_backward: str
-            # custom calib static jump to have static pathes work for all layouts
-            calib_jump_back: list[tuple[float, float]]
 
         path_arr = [
-            PathData(450, None, "arc_top_right", [(500, 40)], 452, "arc_bottom_left", [(20, 360)]),
-            PathData(453, [(20, 20)], "arc_top_left", [(500, 40)], 454, "arc_bottom_right", [(1250,700)]),
-            PathData(456, [(1250, 700)], "arc_bottom_right", [(500, 40)], 457, "arc_top_left", [(20, 20)]),
-            PathData(459, [(20, 700)], "arc_bottom_left", [(500, 20), (700, 100)], None, None, None)
+            PathData(450, None, "arc_top_right", [(500, 40)]),
+            PathData(453, [(20, 20)], "arc_top_left", [(500, 40)]),
+            PathData(456, [(1250, 700)], "arc_bottom_right", [(500, 40)]),
+            PathData(459, [(20, 700)], "arc_bottom_left", [(500, 20), (700, 100)])
         ]
 
         picked_up_items = False
-        for data in path_arr:
-            if do_pre_buff:
-                self._char.pre_buff()
+        self.used_tps = 0
+        if do_pre_buff:
+            self._char.pre_buff()
+
+        for i, data in enumerate(path_arr):
             # calibrating at start and moving towards the end of the arm
             self._pather.traverse_nodes([data.calib_node_start], self._char, force_tp=True)
             if data.calib_jump_start is not None:
@@ -111,9 +98,14 @@ class Arcane:
             picked_up_items |= self._pickit.pick_up_items(self._char)
             if found:
                 return (Location.A2_ARC_END, picked_up_items)
-            elif data.static_path_backward is not None:
-                # Move back to waypoint
-                if not self._return_to_wp(data.calib_node_back, data.static_path_backward, data.calib_jump_back):
+            elif i < len(path_arr) - 1:
+                # Open TP and return back to town, walk to wp and start over
+                if not self._char.tp_town():
+                    return False
+                self.used_tps += 1
+                if not self._town_manager.wait_for_tp(Location.A2_TP):
+                    return False
+                if not self.approach(Location.A2_FARA_STASH):
                     return False
         return False
 
@@ -132,4 +124,4 @@ if __name__ == "__main__":
     screen = Screen(config.general["monitor"])
     game_stats = GameStats()
     bot = Bot(screen, game_stats, False)
-    print(bot._arcane._return_to_wp(454, "arc_bottom_right", [(1250,700)]))
+    bot._arcane._find_summoner([(500, 40)])
