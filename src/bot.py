dd18ff6dd75643dc56125ea52eff276fae13820a
egut125 <egut125@gmail.com>
2022-01-01 14:32:19 -0500

    maye last removal? Probably not

--- src/bot.py
+++ src/bot.py
@@ -46,7 +46,7 @@ class Bot:
         self._ui_manager = UiManager(self._screen, self._template_finder, self._game_stats)
         self._belt_manager = BeltManager(self._screen, self._template_finder)
         self._pather = Pather(self._screen, self._template_finder)
-        self._pickit = PickIt(self._screen, self._item_finder, self._ui_manager, self._belt_manager, self._game_stats)
+        self._pickit = PickIt(self._screen, self._item_finder, self._ui_manager, self._belt_manager)
 
         # Create Character
         if self._config.char["type"] in ["sorceress", "light_sorc"]:
