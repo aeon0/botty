ba70d2b1f2d4550ff632109949213d25c0f2c1e3
egut125 <egut125@gmail.com>
2022-01-01 15:08:16 -0500

    Simple embeds set up

--- src/api/discord_embeds.py
+++ src/api/discord_embeds.py
@@ -2,7 +2,6 @@ from config import Config
 import discord
 from discord import Webhook, RequestsWebhookAdapter, Color
 
-
 class DiscordEmbeds:
     def __init__(self):
         self._config = Config()
@@ -11,16 +10,16 @@ class DiscordEmbeds:
         webhook = Webhook.from_url(self._config.general['custom_message_hook'], adapter=RequestsWebhookAdapter())
 
         if msgData["type"] == "item":             
-            e = discord.Embed(title=f"{self._config.general['name']} found an item", description=f"{msgData['item']}", color=Color.dark_gold())
+            e = discord.Embed(title=f"{self._config.general['name']} found an item", description=f"{msgData['item']} at {msgData['location']}", color=self.get_Item_Color( msgData['item']))
             # file = discord.File(msgData['image'], filename="image.png")
             # e.set_image(url="attachment://image.png")
         elif msgData["type"] == "death":
             msg = f"{self._config.general['name']}: You have died at {msgData['location']}"
-            e = discord.Embed(title=f"{self._config.general['name']} has died", description=f"{msgData['item']}", color=Color.dark_red())
+            e = discord.Embed(title=f"{self._config.general['name']} has died", color=Color.dark_red())
             e.add_field(name="Location", value=msgData['location'])
 
         elif msgData["type"] == "chicken":
-            e = discord.Embed(title=f"{self._config.general['name']} has chickened", description=f"{msgData['item']}", color=Color.dark_grey())
+            e = discord.Embed(title=f"{self._config.general['name']} has chickened", color=Color.dark_grey())
             e.add_field(name="Location", value=msgData['location'])
 
         elif msgData["type"] == "message":
@@ -28,3 +27,17 @@ class DiscordEmbeds:
 
         webhook.send(embed=e)
         # webhook.send(embed=e, file=file)
+
+    def get_Item_Color(self, item):
+        if "magic_" in item:
+            return Color.blue()
+        elif "set_" in item:
+            return Color.green()
+        elif "rune_" in item:
+            return Color.gold()
+        elif "uniq_" in item or "rare" in item:
+            return Color.dark_gold()
+        elif "gray_" in item:
+            return Color.darker_grey()
+        else:
+            return Color.blue()
