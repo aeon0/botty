c85639482a1d4a47c484fcb6fe590f7452dc2aa9
famE <ralf.boesche@gmx.de>
2022-01-04 18:33:36 +0100

    reset bot init value

--- src/bot.py
+++ src/bot.py
@@ -109,7 +109,7 @@ class Bot:
         self._stopping = False
         self._pausing = False
         self._current_threads = []
-        self._no_stash_counter = 5
+        self._no_stash_counter = 0
         self._ran_no_pickup = False
 
         # Create State Machine
