d9951329c0d23ef3882fa1e9a32904a2925f6776
aeon0 <aeon4@pm.me>
2022-01-05 11:42:34 +0100

    Test utils misc load_template() (#396)

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -77,9 +77,13 @@ def hms(seconds: int):
 
 def load_template(path, scale_factor: float = 1.0, alpha: bool = False):
     if os.path.isfile(path):
-        template_img = cv2.imread(path, cv2.IMREAD_UNCHANGED) if alpha else cv2.imread(path)
-        template_img = cv2.resize(template_img, None, fx=scale_factor, fy=scale_factor, interpolation=cv2.INTER_NEAREST)
-        return template_img
+        try:
+            template_img = cv2.imread(path, cv2.IMREAD_UNCHANGED) if alpha else cv2.imread(path)
+            template_img = cv2.resize(template_img, None, fx=scale_factor, fy=scale_factor, interpolation=cv2.INTER_NEAREST)
+            return template_img
+        except Exception as e:
+            print(e)
+            raise ValueError(f"Could not load template: {path}")
     return None
 
 def alpha_to_mask(img: np.ndarray):
