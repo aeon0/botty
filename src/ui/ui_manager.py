5073f23934239b98b6ecaf3dd37143661ab3e5b5
bjoerm <bjoerm@users.noreply.github.com>
2022-01-06 18:35:17 +0100

    If super potions are not available (e.g. nightmare), buy greater potions. (#401)

--- src/ui/ui_manager.py
+++ src/ui/ui_manager.py
@@ -586,18 +586,23 @@ class UiManager():
 
     def buy_pots(self, healing_pots: int = 0, mana_pots: int = 0):
         """
-        Buy pots from Malah or Ormus. Vendor inventory needs to be open!
+        Buy pots from vendors. Vendor inventory needs to be open!
         :param healing_pots: Number of healing pots to buy
         :param mana_pots: Number of mana pots to buy
         """
         h_pot = self._template_finder.search_and_wait("SUPER_HEALING_POTION", roi=self._config.ui_roi["vendor_stash"], time_out=3)
+        if h_pot.valid is False:  # If not available in shop, try to shop next best potion.
+            h_pot = self._template_finder.search_and_wait("GREATER_HEALING_POTION", roi=self._config.ui_roi["vendor_stash"], time_out=3)
         if h_pot.valid:
             x, y = self._screen.convert_screen_to_monitor(h_pot.position)
             mouse.move(x, y, randomize=8, delay_factor=[1.0, 1.5])
             for _ in range(healing_pots):
                 mouse.click(button="right")
                 wait(0.9, 1.1)
+
         m_pot = self._template_finder.search_and_wait("SUPER_MANA_POTION", roi=self._config.ui_roi["vendor_stash"], time_out=3)
+        if m_pot.valid is False:  # If not available in shop, try to shop next best potion.
+            m_pot = self._template_finder.search_and_wait("GREATER_MANA_POTION", roi=self._config.ui_roi["vendor_stash"], time_out=3)
         if m_pot.valid:
             x, y = self._screen.convert_screen_to_monitor(m_pot.position)
             mouse.move(x, y, randomize=8, delay_factor=[1.0, 1.5])
