8dc55df53cb40d2fc477f1257cafd89a91daccb3
aeon0 <aeon4@pm.me>
2022-01-07 19:26:08 +0100

    make it fancy

--- src/ui/char_selector.py
+++ src/ui/char_selector.py
@@ -2,7 +2,7 @@ from config import Config
 from utils.custom_mouse import mouse
 from utils.misc import cut_roi, wait
 from screen import Screen
-from template_finder import TemplateFinder, TemplateMatch
+from template_finder import TemplateFinder
 
 
 class CharSelector:
@@ -23,28 +23,29 @@ class CharSelector:
     def select_char(self):
         if self._char_template is not None:
             scrolls_attempts = 0
-            template_result = TemplateMatch()
-            coords = self._config.ui_roi["char_selection_all"]
-            while not template_result.valid and scrolls_attempts < 2:  # 2 scrolls should suffice to see all possible characters
+            while scrolls_attempts < 2:  # 2 scrolls should suffice to see all possible characters
                 template_result = self._template_finder.search(
                     self._char_template,
                     self._screen.grab(),
                     threshold=0.8,
-                    roi=coords, normalize_monitor=True
+                    roi=self._config.ui_roi["char_selection_all"],
+                    normalize_monitor=True
                 )
-                if not template_result.valid:
+                if template_result.valid:
+                    # move cursor to result and select
+                    mouse.move(*template_result.position)
+                    wait(0.5)
+                    mouse.click(button="left")
+                    break
+                else:
                     # We can scroll the characters only if we have the mouse in the char names selection so move the mouse there
-                    pos = self._screen.convert_screen_to_monitor((coords[0], coords[1]))
+                    pos = (self._config.ui_pos["char_selection_center_x"], self._config.ui_pos["char_selection_center_y"])
+                    pos = self._screen.convert_screen_to_monitor(pos)
                     mouse.move(*pos)
                     wait(0.5)
                     mouse.wheel(-14)
                     scrolls_attempts += 1
                     wait(0.5)
-                else:
-                    # move cursor to result and select
-                    mouse.move(*template_result.position)
-                    wait(0.5)
-                    mouse.click(button="left")
         return False
 
 
@@ -57,5 +58,4 @@ if __name__ == "__main__":
     selector = CharSelector(screen, config)
     if not selector.has_char_template_saved():
         selector.save_char_template()
-    keyboard.wait("f11")
     selector.select_char()
