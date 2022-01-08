eed5028ae51198038f8ba288f61046d6edef99dd
John Wu <33677676+JohnStyleZ@users.noreply.github.com>
2022-01-08 11:30:06 -0800

    added option to refill keys

--- src/bot.py
+++ src/bot.py
@@ -273,10 +273,14 @@ class Bot:
 
         # Check if we are out of tps or need repairing
         need_repair = self._ui_manager.repair_needed()
-        if self._tps_left < random.randint(3, 5) or need_repair or self._config.char["always_repair"]:
+        if self._tps_left < random.randint(3, 5) or need_repair or self._config.char["always_repair"] or self._config.char["always_refill_keys"]:
             if need_repair: Logger.info("Repair needed. Gear is about to break")
             else: Logger.info("Repairing and buying TPs at next Vendor")
-            self._curr_loc = self._town_manager.repair_and_fill_tps(self._curr_loc)
+            if self._config.char["always_refill_keys"]:
+                self._curr_loc = self._town_manager.refill_keys(self._curr_loc)
+                if not self._curr_loc:
+                	return self.trigger_or_stop("end_game", failed=True)
+            else: self._curr_loc = self._town_manager.repair_and_fill_tps(self._curr_loc)
             if not self._curr_loc:
                 return self.trigger_or_stop("end_game", failed=True)
             self._tps_left = 20
