e874ad10632c02067fb79f2424a5327d2aa27f5e
John Wu <33677676+JohnStyleZ@users.noreply.github.com>
2022-01-11 06:49:45 -0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -121,7 +121,7 @@ class Config:
             "atk_len_shenk": float(self._select_val("char", "atk_len_shenk")),
             "atk_len_nihlatak": float(self._select_val("char", "atk_len_nihlatak")),
             "always_repair": bool(int(self._select_val("char", "always_repair"))),
-            "always_refill_keys": bool(int(self._select_val("char", "always_refill_keys"))),
+            "buy_keys": bool(int(self._select_val("char", "buy_keys"))),
         }
 
         # Sorc base config
