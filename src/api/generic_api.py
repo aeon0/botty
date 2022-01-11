278fdbd2112def6ae0433cd2261c1752fd9ae1c2
aeon0 <aeon4@pm.me>
2022-01-11 09:19:43 +0100

    Revert "Messaging update (#408)" (#425)
    This reverts commit d280a3b4db2344d0670a661e470e778f1eec36b8.

--- src/api/generic_api.py
+++ src/api/generic_api.py
@@ -27,10 +27,7 @@ class GenericApi:
         msg = f"All stash is full, quitting"
         self._send(msg)
 
-    def send_status(self, msg: str):
-        self._send(msg)
-
-    def send_message(self, msg: str, img=""):
+    def send_message(self, msg: str):
         self._send(msg)
 
     def _send(self, msg: str):
