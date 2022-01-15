f79688e00f611488d008d933e54882b3519c004e
aeon0 <aeon4@pm.me>
2022-01-15 11:06:41 +0100

    Hot fix (#454)
    * fix pather timeout
    
    * revert changes

--- src/pather.py
+++ src/pather.py
@@ -547,7 +547,6 @@ class Pather:
         if do_pre_move:
             char.pre_move()
         last_direction = None
-        last_known_good_pos = None
         for i, node_idx in enumerate(path):
             continue_to_next_node = False
             last_move = time.time()
@@ -567,15 +566,9 @@ class Pather:
                         # because of all the spells and monsters it often can not determine the final template
                         # Don't want to spam the log with errors in this case because it most likely worked out just fine
                         if time_out > 3.1:
-                            if last_known_good_pos is not None:
-                                Logger.error("One final try before totally stuck")
-                                x_m, y_m = self._screen.convert_abs_to_monitor(last_known_good_pos)
-                                char.move((x_m, y_m))
-                                last_known_good_pos = None
-                            else:
-                                if self._config.general["info_screenshots"]:
-                                    cv2.imwrite("./info_screenshots/info_pather_got_stuck_" + time.strftime("%Y%m%d_%H%M%S") + ".png", img)
-                                Logger.error("Got stuck exit pather")
+                            if self._config.general["info_screenshots"]:
+                                cv2.imwrite("./info_screenshots/info_pather_got_stuck_" + time.strftime("%Y%m%d_%H%M%S") + ".png", img)
+                            Logger.error("Got stuck exit pather")
                         return False
 
                 # Sometimes we get stuck at rocks and stuff, after a few seconds force a move into the last known direction
@@ -601,7 +594,6 @@ class Pather:
                         x_m, y_m = self._screen.convert_abs_to_monitor(node_pos_abs)
                         char.move((x_m, y_m), force_tp=force_tp, force_move=force_move)
                         last_direction = node_pos_abs
-                        last_known_good_pos = node_pos_abs
                         last_move = time.time()
 
         return True
