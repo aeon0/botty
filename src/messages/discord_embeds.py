67e81009e43c84526e83cf91e8895f6b142e3229
egut125 <egut125@gmail.com>
2022-02-04 23:28:28 -0500

    Update src/messages/discord_embeds.py

--- src/messages/discord_embeds.py
+++ src/messages/discord_embeds.py
@@ -63,7 +63,7 @@ class DiscordEmbeds():
         e.set_thumbnail(url=f"{self._psnURL}6L341955.png")
         self._send_embed(e)
 
-    def send_status(self, title: str, msg: str, img:str):
+    def send_status(self, title: str, msg: str):
         e = discord.Embed(title=f"{title}", description=f"```{msg}```", color=Color.dark_teal())
         if not self._config.general['discord_status_condensed']:
             e.set_thumbnail(url=f"{self._psnURL}36L4a4994.png")
