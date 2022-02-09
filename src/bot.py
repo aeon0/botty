5625300b9f0a365c46aeacecccda3098c3611314
jobithu <95491220+jobithu@users.noreply.github.com>
2022-02-09 22:09:19 +0100

    small fix before merging

--- src/bot.py
+++ src/bot.py
@@ -57,11 +57,11 @@ class Bot:
 
         # Create Character
         if self._config.char["type"] in ["sorceress", "light_sorc"]:
-            self._char: IChar = LightSorc(self._config.light_sorc, self._screen, self._template_finder, self._ui_manager, self._pather, self._pickit) #pickit added for diablo)
+            self._char: IChar = LightSorc(self._config.light_sorc, self._screen, self._template_finder, self._ui_manager, self._pather)
         elif self._config.char["type"] == "blizz_sorc":
-            self._char: IChar = BlizzSorc(self._config.blizz_sorc, self._screen, self._template_finder, self._ui_manager, self._pather, self._pickit) #pickit added for diablo) 
+            self._char: IChar = BlizzSorc(self._config.blizz_sorc, self._screen, self._template_finder, self._ui_manager, self._pather)
         elif self._config.char["type"] == "nova_sorc":
-            self._char: IChar = NovaSorc(self._config.nova_sorc, self._screen, self._template_finder, self._ui_manager, self._pather, self._pickit) #pickit added for diablo)
+            self._char: IChar = NovaSorc(self._config.nova_sorc, self._screen, self._template_finder, self._ui_manager, self._pather)
         elif self._config.char["type"] == "hammerdin":
             self._char: IChar = Hammerdin(self._config.hammerdin, self._screen, self._template_finder, self._ui_manager, self._pather, self._pickit) #pickit added for diablo
         elif self._config.char["type"] == "trapsin":
@@ -297,7 +297,7 @@ class Bot:
         # Check if we should force stash (e.g. when picking up items by accident or after failed runs or chicken/death)
         force_stash = False
         self._no_stash_counter += 1
-        if not self._picked_up_items and (self._no_stash_counter > self._config.char["runs_per_repair"] or self._pick_corpse):
+        if not self._picked_up_items and (self._no_stash_counter > self._config.char["runs_per_stash"] or self._pick_corpse):
             self._no_stash_counter = 0
             force_stash = self._ui_manager.should_stash(self._config.char["num_loot_columns"])
         # Stash stuff, either when item was picked up or after X runs without stashing because of unwanted loot in inventory
