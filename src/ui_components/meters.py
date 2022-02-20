e3baebeae8e100f210cd2ad44093e3f038601364
mgleed <aliigleed16@gmail.com>
2022-02-20 12:50:34 -0500

    fix meters, simplify health/mana check

--- src/ui_components/meters.py
+++ src/ui_components/meters.py
@@ -4,20 +4,18 @@ from utils.misc import cut_roi, color_filter
 from config import Config
 
 def get_health(img: np.ndarray) -> float:
-    health_img = cut_roi(img, Config().ui_roi["health_slice"])
     # red mask
-    mask1, _ = color_filter(health_img, [np.array([0, 110, 20]), np.array([2, 255, 255])])
-    mask2, _ = color_filter(health_img, [np.array([178, 110, 20]), np.array([180, 255, 255])])
-    mask = cv2.bitwise_or(mask1, mask2)
+    health_img = cut_roi(img, Config().ui_roi["health_slice"])
+    mask, _ = color_filter(health_img, Config().colors["health_globe_red"])
     health_percentage = (float(np.sum(mask)) / mask.size) * (1/255.0)
     # green (in case of poison)
-    mask, _ = color_filter(health_img, [np.array([47, 90, 20]), np.array([54, 255, 255])])
+    mask, _ = color_filter(health_img, Config().colors["health_globe_green"])
     health_percentage_green = (float(np.sum(mask)) / mask.size) * (1/255.0)
     return max(health_percentage, health_percentage_green)
 
 def get_mana(img: np.ndarray) -> float:
     mana_img = cut_roi(img, Config().ui_roi["mana_slice"])
-    mask, _ = color_filter(mana_img, [np.array([117, 120, 20]), np.array([121, 255, 255])])
+    mask, _ = color_filter(mana_img, Config().colors["mana_globe"])
     mana_percentage = (float(np.sum(mask)) / mask.size) * (1/255.0)
     return mana_percentage
 
