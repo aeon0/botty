f8d14484671903d906dcec81f64e0605c5fb7af9
Ezro <8863899+Ezro@users.noreply.github.com>
2022-03-09 21:15:36 -0700

    Bugfix: Prevent join for NoneType and start_detecting_window() in main.py (#624)
    * Prevent join for NoneType and start_detecting_window() in main.py
    
    * Start and stop in the debugger toggle

--- src/main.py
+++ src/main.py
@@ -26,14 +26,16 @@ def start_or_pause_bot(controllers: Controllers):
     else:
         # Kill any other controllers and start botty
         controllers.debugger.stop()
-        controllers.game.start()
         screen.start_detecting_window()
+        controllers.game.start()
 
 def start_or_stop_graphic_debugger(controllers: Controllers):
     if controllers.debugger.is_running:
         controllers.debugger.stop()
+        screen.stop_detecting_window()
     else:
         # Kill any other controller and start debugger
+        screen.start_detecting_window()
         controllers.game.stop()
         controllers.debugger.start()
 
