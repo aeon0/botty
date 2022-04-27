161d663ff4185f4755db2ebe0a1a962c62889505
flustered <hsomervi5790@gmail.com>
2022-04-27 17:40:34 -0400

    Filter items and discord notifications with new NIP system.

--- src/game_stats.py
+++ src/game_stats.py
@@ -49,13 +49,16 @@ class GameStats:
             self._location_stats[self._location] = { "items": [], "deaths": 0, "chickens": 0, "merc_deaths": 0, "failed_runs": 0 }
 
     def log_item_keep(self, item_name: str, send_message: bool, img: np.ndarray, ocr_text: str = '', expression: str = ''):
-        Logger.debug(f"Stashed and logged: {item_name}")
-        filtered_items = ["_potion", "misc_gold", "_amethyst", "_ruby", "misc_chipped_diamond", "misc_flawed_diamond", "misc_diamond", "misc_flawless_diamond", "_topaz", "_emerald", "_sapphire", "misc_chipped_skull", "misc_flawed_skull", "misc_skull", "misc_flawless_skull"]
-        if self._location is not None and not any(substring in item_name for substring in filtered_items):
+        filtered_substrings = [" POTION", " OF IDENTIFY", " OF TOWN PORTAL", " AMETHYST", " RUBY", " TOPAZ", " EMERALD", " SAPPHIRE", " DIAMOND"]
+        filtered_matches = ["DIAMOND", "AMETHYST", "RUBY", "TOPAZ", "EMERALD", "SAPPHIRE", "ARROWS", "BOLTS",
+                            "CHIPPED SKULL", "FLAWED SKULL", "SKULL", "FLAWLESS SKULL", "PERFECT SKULL"]
+        skip_log = any(substring in item_name for substring in filtered_substrings) or any(match == item_name.strip() for match in filtered_matches)
+        if self._location is not None and not skip_log:
+            Logger.debug(f"Stashed and logged: {item_name}")
             self._location_stats[self._location]["items"].append(item_name)
             self._location_stats["totals"]["items"] += 1
 
-        if send_message:
+        if send_message and not skip_log:
             self._messenger.send_item(item_name, img, self._location, ocr_text, expression)
 
     def log_death(self, img: str):
