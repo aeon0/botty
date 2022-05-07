cc2730b146f61cb042edcd65bcfee5159254f67f
mgleed <aliigleed16@gmail.com>
2022-05-07 18:10:16 -0400

    update tests

--- test/d2r_image/test_get_hovered_item.py
+++ test/d2r_image/test_get_hovered_item.py
@@ -6,16 +6,13 @@ from d2r_image.data_models import HoveredItem
 
 
 @pytest.mark.parametrize("filename, expected_file", [
-    ('cold_facet_die.png', 'cold_facet_die.json'),
-    ('fire_facet_die.png', 'fire_facet_die.json'),
-    ('psn_facet_lvlup.png', 'psn_facet_lvlup.json'),
-    ('rare_orb_sell_larzuk.png', 'rare_orb_sell_larzuk.json'),
-    ('rare_ring.png', 'rare_ring.json'),
-    ('spirit.png', 'spirit.json'),
-    ('torch.png', 'torch.json'),
-    ('unid_rare_dagger.png', 'unid_rare_dagger.json'),
-    ('war_travs.png', 'war_travs.json'),
-    ('zod.png', 'zod.json'),
+    ('hovered_item_20220504_160228.png', 'hovered_item_20220504_160228.json'),
+    ('hovered_item_20220504_160419.png', 'hovered_item_20220504_160419.json'),
+    ('hovered_item_20220504_161007.png', 'hovered_item_20220504_161007.json'),
+    ('hovered_item_20220504_161026.png', 'hovered_item_20220504_161026.json'),
+    ('hovered_item_20220504_161113.png', 'hovered_item_20220504_161113.json'),
+    ('hovered_item_20220504_161131.png', 'hovered_item_20220504_161131.json'),
+    ('hovered_item_20220504_161338.png', 'hovered_item_20220504_161338.json'),
 ])
 def test_hovered_item(filename: str, expected_file: str):
     image_path = os.path.join(
