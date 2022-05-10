4776d001e56155cfe12e27229155b6269257ddc8
mgleed <aliigleed16@gmail.com>
2022-05-10 18:24:12 -0400

    consolidate tests

--- test/target_detect_test.py
+++ test/target_detect_test.py
@@ -2,16 +2,10 @@ from target_detect import mob_check
 import cv2
 import pytest
 
-@pytest.mark.parametrize("screen_path", (
-    "test/assets/mobs.png",
-))
-def test_mob_detect(screen_path):
+@pytest.mark.parametrize("screen_path, expected_res", [
+    ("test/assets/mobs.png", True),
+    ("test/assets/mobs_no_green_or_blue.png", False),
+])
+def test_mob_detect(screen_path, expected_res):
     img = cv2.imread(screen_path)
-    assert mob_check(img)
-
-@pytest.mark.parametrize("screen_path", (
-    "test/assets/mobs_no_green_or_blue.png",
-))
-def test_no_mobs(screen_path):
-    img = cv2.imread(screen_path)
-    assert mob_check(img) == 0
+    assert bool(mob_check(img)) == expected_res
