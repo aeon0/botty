044608a1ea0a2b7686e5060ad65388b334cd1a20
mgleed <aliigleed16@gmail.com>
2022-05-13 16:15:43 -0400

    Bugfix: Fix local pather pytest, update log to accommodate mob detect change (#766)
    * init
    
    * fix
    
    * refactor

--- test/target_detect_test.py
+++ test/target_detect_test.py
@@ -1,11 +1,13 @@
 from target_detect import mob_check
 import cv2
 import pytest
+import screen
 
 @pytest.mark.parametrize("screen_path, expected_res", [
     ("test/assets/mobs.png", True),
     ("test/assets/mobs_no_green_or_blue.png", False),
 ])
 def test_mob_detect(screen_path, expected_res):
+    screen.set_window_position(0, 0)
     img = cv2.imread(screen_path)
     assert bool(mob_check(img)) == expected_res
