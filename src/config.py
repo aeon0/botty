a526dec6ec7f138700312e03dcc2cf46eb9eb4d6
Legit <96157236+D2RLegit@users.noreply.github.com>
2022-05-30 17:47:55 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -300,6 +300,7 @@ class Config:
             "runs_per_repair": False if not self._select_val("char", "runs_per_repair") else int(self._select_val("char", "runs_per_repair")),
             "gamble_items": False if not self._select_val("char", "gamble_items") else self._select_val("char", "gamble_items").replace(" ","").split(","),
             "sell_junk": bool(int(self._select_val("char", "sell_junk"))),
+            "enable_no_pickup": bool(int(self._select_val("char", "enable_no_pickup"))),
         }
         # Sorc base config
         sorc_base_cfg = dict(self.configs["config"]["parser"]["sorceress"])
