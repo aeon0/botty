7bd65ad8a164ab10061e275aaec4148f610a1d74
Riddle <75004732+Aimware0@users.noreply.github.com>
2022-06-06 19:23:28 -0400

    Revert "Cleaned up more of lexer.py (WIP)"
    This reverts commit 6ae352eedd6ad2c5d709409134824de2b2cb8242.

--- src/nip/lexer.py
+++ src/nip/lexer.py
@@ -40,9 +40,6 @@ class Lexer:
         self.tokens: List = []
 
     def increment_section(self):
-        """
-            Increments the self.current_section to the next section.
-        """
         if self.current_section == NipSections.PROP:
             self.current_section = NipSections.STAT
         elif self.current_section == NipSections.STAT:
@@ -50,9 +47,6 @@ class Lexer:
 
 
     def _advance(self):
-        """
-            Advances the self.text_i and self.current_token.
-        """
         try:
             self.text_i += 1
             self.current_token = self.text[self.text_i]
@@ -64,16 +58,12 @@ class Lexer:
             Returns the self.text in a string type, and at its current iteration.
         """
         return "".join(self.text[self.text_i:])
- 
-       
-    def create_tokens(self, nip_expression, starting_section=NipSections.PROP):
-        """
-            Creates the tokens for the nip_expression.
-        """
+
+
+    def create_tokens(self, nip_expression):
         self.text = list(nip_expression)
         self._advance()
         self.tokens = []
-        self.current_section = starting_section
         while self.current_token != None:
             if self.current_token in DIGITS:
                 self.tokens.append(self._create_digits())
@@ -93,37 +83,30 @@ class Lexer:
         return self.tokens
 
     def _create_digits(self):
-        """
-            Creates a token for a digit.
-        """
         dot_count = 0
         n_str = self.current_token
         self._advance()
-        if n_str:
-            while self.current_token != None and self.current_token in DIGITS:
-                if self.current_token == ".":
-                    if dot_count >= 1:
-                        break
-                    dot_count += 1
-                n_str += self.current_token
-                if self.current_token == "%":
-                    self._advance()
+        while self.current_token != None and self.current_token in DIGITS:
+            if self.current_token == ".":
+                if dot_count >= 1:
                     break
+                dot_count += 1
+            n_str += self.current_token
+            if self.current_token == "%":
                 self._advance()
-            if n_str.startswith("."):
-                n_str = "0" + n_str
-            elif n_str.endswith("."):
-                n_str = n_str + "0"
-            elif n_str.endswith("%"):
-                if n_str != None:
-                    return Token(TokenType.NUMBERPERCENT, n_str[:-1])
-            return Token(TokenType.NUMBER, float(n_str))
+                break
+            self._advance()
+        if n_str.startswith("."):
+            n_str = "0" + n_str
+        elif n_str.endswith("."):
+            n_str = n_str + "0"
+        elif n_str.endswith("%"):
+            if n_str != None:
+                return Token(TokenType.NUMBERPERCENT, n_str[:-1])
+        return Token(TokenType.NUMBER, float(n_str))
 
 
     def _create_math_operator(self):
-        """
-            Creates a token for a math operator.
-        """
         symbol = self.current_token
         self._advance()
         symbol_map = {
@@ -145,72 +128,77 @@ class Lexer:
                 if symbol:
                     return Token(symbol_map[symbol], symbol)
 
+        
+
 
     def _create_nip_lookup(self):
         """
-            Create a token for a nip lookup.
             item data lookup i.e [name]
         """
-
         self._advance() # Increment past the [
         lookup_key = ""
-
         # Now it should look something like this "lookup_key]"
 
         if self.text:
             found_match = re.match(r"(\w+])|(\d+)]", self.get_current_iteration_of_text_raw()) # Find the lookup_key]
             if found_match and found_match.group(1):
-                found = found_match.group(1)#.replace("]", "") # Remove the closing bracket.
+                found = found_match.group(1).replace("]", "") # Remove the closing bracket.
                 for i in range(len(found)):
-                    if found[i] != "]": # * We don't want to add the closing bracket in our lookup_key...
-                        lookup_key += found[i]
+                    lookup_key += found[i]
                     self._advance()
 
-        allowed_NTIPAliases = { # * Allows NTIP operators to be used within certain sections.
-            # * For example, [name] is allowed in the PROP section, and is [flag] too. Also everything inside NTIPAliasClass, NTIPAliasQuality dicts ect is allowed inside props.
-            NipSections.PROP: [
-                ["name", TokenType.NAME],
-                ["flag", TokenType.FLAG],
-                ["class", TokenType.CLASS],
-                ["quality", TokenType.QUALITY],
-                ["type", TokenType._TYPE],
-                ["idname", TokenType.IDNAME],
+        self._advance() # Increment past the ]
 
-                [NTIPAliasClass, TokenType.NTIPAliasClass],
-                [NTIPAliasQuality, TokenType.NTIPAliasQuality],
-                [NTIPAliasClassID, TokenType.NTIPAliasClassID],
-                [NTIPAliasFlag, TokenType.NTIPAliasFlag],
-                [NTIPAliasType, TokenType.NTIPAliasType],
-            ],
+        if self.current_section == NipSections.PROP:
+            if lookup_key:
+                match lookup_key:
+                    case "name":
+                        return Token(TokenType.NAME, lookup_key)
+                    case "flag":
+                        return Token(TokenType.FLAG, lookup_key)
+                    case "class":
+                        return Token(TokenType.CLASS, lookup_key)
+                    case "quality":
+                        return Token(TokenType.QUALITY, lookup_key)
+                    case "type":
+                        return Token(TokenType._TYPE, lookup_key)
+                    case "idname":
+                        return Token(TokenType.IDNAME, lookup_key)
+                    case _: # ? This is default.. 
+                        if lookup_key in NTIPAliasClass:
+                            return Token(TokenType.NTIPAliasClass, NTIPAliasClass[lookup_key])
+                        elif lookup_key in NTIPAliasQuality:
+                            return Token(TokenType.NTIPAliasQuality, NTIPAliasQuality[lookup_key])
+                        elif lookup_key in NTIPAliasClassID:
+                            return Token(TokenType.NTIPAliasClassID, NTIPAliasClassID[lookup_key])
+                        elif lookup_key in NTIPAliasFlag:
+                            return Token(TokenType.NTIPAliasFlag, NTIPAliasFlag[lookup_key])
+                        elif lookup_key in NTIPAliasType:
+                            return Token(TokenType.NTIPAliasType, NTIPAliasType[lookup_key])
+                        return Token(TokenType.UNKNOWN, "-1")
+    
 
-            NipSections.STAT: [
-                [NTIPAliasStat, TokenType.NTIPAliasStat]
-            ],
-
-            NipSections.MAXQUANTITY: [
-                ["maxquanity", TokenType.MAXQUANITY]
-            ]
-        }
-            
-        if allowed_NTIPAliases[self.current_section]:
-            for alias in allowed_NTIPAliases[self.current_section]:
-                if isinstance(alias[0], str):
-                    if lookup_key == alias[0]:
-                        return Token(alias[1], lookup_key)
-                elif isinstance(alias[0], dict):
-                    if lookup_key in alias[0]:
-                        return Token(alias[1], alias[0][lookup_key])
+        elif self.current_section == NipSections.STAT:
+            if lookup_key in NTIPAliasStat:
+                return Token(TokenType.NTIPAliasStat, NTIPAliasStat[lookup_key])
+            else:
+                return Token(TokenType.UNKNOWN, "-1")
+        elif self.current_section == NipSections.MAXQUANTITY:
+            if lookup_key == "maxquanity":
+                return Token(TokenType.MAXQUANITY, lookup_key)
+            else:
+                return Token(TokenType.UNKNOWN, "-1")
 
     def _create_d2r_image_data_lookup(self):
-        lookup_key = ""
-        found_match = re.match(r"(\w+)\s*", self.get_current_iteration_of_text_raw()) # Look at chars until we find a space.
-
-        if found_match and found_match.group(1):
-            found = found_match.group(1)
-            for i in range(len(found)):
-                lookup_key += found[i]
-                self._advance()
-
+        lookup_key = self.current_token
+        while self.current_token != None:
+            self._advance()
+            if self.current_token == None or self.current_token not in CHARS:
+                break
+            if self.current_token == "'":
+                self.current_token = "\\'" # TODO FIX THIS (make stuff like diablo'shorn work..)
+            if lookup_key and self.current_token:
+                lookup_key += self.current_token
         # Converts stuff like ethereal to NTIPAliasFlag['ethereal']
         if self.current_section == NipSections.PROP:
             if lookup_key in NTIPAliasClass and self.tokens[-2].type == TokenType.CLASS:
@@ -231,6 +219,8 @@ class Lexer:
             else:
                 return Token(TokenType.UNKNOWN, "-1")
 
+
+
     def _create_logical_operator(self):
         char = self.current_token
         self._advance()
@@ -247,7 +237,7 @@ class Lexer:
             "&&": TokenType.AND,
             "||": TokenType.OR,
 
-            "#": TokenType.SECTIONAND 
+            "#": TokenType.SECTIONAND
         }
 
         pattern = r"(>=|<=|==|!=|&&|\|\||>|<|\#)"
@@ -279,7 +269,9 @@ class Lexer:
                     self._advance()
                 if operator == "#":
                     self.increment_section()
+
                 pythonic_relation_operator = operator.replace("&&", "and").replace("||", "or")
+
                 return Token(logical_operator_map[operator], pythonic_relation_operator)
         else:
             raise NipSyntaxError(f"Unexpected logical operator {invalid_char}")
