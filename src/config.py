c1e51db03b8540252ca3be6280e577446e159fb3
jobithu <95491220+jobithu@users.noreply.github.com>
2022-06-11 20:57:27 +0200

    Fixed config error. Set default to 0

--- src/config.py
+++ src/config.py
@@ -307,7 +307,7 @@ class Config:
             "gamble_items": False if not self._select_val("char", "gamble_items") else self._select_val("char", "gamble_items").replace(" ","").split(","),
             "sell_junk": bool(int(self._select_val("char", "sell_junk"))),
             "enable_no_pickup": bool(int(self._select_val("char", "enable_no_pickup"))),
-            "prefer_act4_town": bool(int(self._select_val("char", "enable_no_pickup"))),
+            "prefer_act4_town": bool(int(self._select_val("char", "prefer_act4_town"))),
         }
         # Sorc base config
         sorc_base_cfg = dict(self.configs["config"]["parser"]["sorceress"])
